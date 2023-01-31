349837f54bf1a34223d9212c0e8f03bd26ed08f4
worasit <worasit.daimongkol@thoughtworks.com>
2023-01-31 20:43:33 +0700

    Added Logout button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginButton from "./login";
+import LogoutButton from "./logout";
 
 function App() {
     return (
@@ -10,14 +11,7 @@ function App() {
                 <p>
                     <LoginButton></LoginButton>
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <LogoutButton></LogoutButton>
             </header>
         </div>
     );
